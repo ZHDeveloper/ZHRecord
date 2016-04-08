3c136884a98024ab6d538c20f680a325cce2ef4a
ZhiHua <zhcoder2011@gmail.com>
2016-04-08 11:22:42 +0800

    Initial Commit

---
+++ RecordTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecordTest
+//
+//  Created by AdminZhiHua on 16/4/8.
+//  Copyright © 2016年 AdminZhiHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
